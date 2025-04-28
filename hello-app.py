d77db97a1e997d3c67cd4c4cb0002aa94f2f0b12
Owaisdxd <804awais@gmail.com>
2025-04-29 02:43:25 +0400

    Second upload to test

---
+++ hello-app.py
@@ -0,0 +1,6 @@
+#!/home/os/anaconda3/bin/python3
+def main():
+    print("Hello from Jenkins!")
+
+if __name__ == "__main__":
+    main()
--- simple-app.py
+++ simple-app.py
@@ -1,2 +1,3 @@
 #!/home/os/anaconda3/bin/python3
 print("Hello World")
+print("Hello world from jenkins")
